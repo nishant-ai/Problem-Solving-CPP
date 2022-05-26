8cbfac252db6d3dc8af56cf56d385a6ed89306c3
Nishant Sharma <nishant-ai@outlook.com>
2022-05-26 21:44:55 +0530

    DP commit-2

--- 003-longest-increasing-subseq.cpp
+++ 003-longest-increasing-subseq.cpp
@@ -23,7 +23,7 @@ int lis(int n){
 const int N = 1e5+10;
 int dp[N];
 
-int lis(int n){
+int lis(int n){ // returns the length only
     if (dp[n] != -1) return dp[n];
     int ans = 1;
 
@@ -33,6 +33,34 @@ int lis(int n){
 
     return dp[n] = ans;
 }
+
+// LEETCODE SOLUTION WITH DP
+class Solution {
+public:
+    int dp[2501];
+    
+    int lis(int n, vector<int>&a){
+        if (dp[n] != -1) return dp[n];
+        int ans = 1;
+
+        for (int i=0; i<n; ++i){
+            if (a[n] > a[i]) ans = max(ans, lis(i, a)+1);
+        }
+
+        return dp[n] = ans;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        int ans = 0;
+        
+        for (int i=0; i<n; i++)
+            ans = max(lis(i, nums), ans);
+        
+        return ans;
+    }
+};
 
 
 int main(){
