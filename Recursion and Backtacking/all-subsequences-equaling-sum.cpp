0f469a9cbd8e9bbc71de79aedeaf28fb592167df
Nishant Sharma <nishant-ai@outlook.com>
2022-05-30 22:13:23 +0530

    print all subseq and one subseq w/ sum

--- Recursion and Backtacking/all-subsequences-equaling-sum.cpp
+++ Recursion and Backtacking/all-subsequences-equaling-sum.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 // Subsequences with a sum mentioned
 
+/*
+if i wanna print only 1 result
+bool subSeqSum(int idx, int target, vector<int>&a, int sum, vector<int>&h){
+    if (idx==a.size()){ 
+        if (sum==target){
+            for (int i: h) cout << i << " ";
+            cout << endl;
+            return true;
+        }
+        return false;
+    }
+    // take
+    sum+=a[idx];
+    h.push_back(a[idx]);
+    if (subSeqSum(idx+1, target, a, sum, h)==true) return true;
+    // Not take
+    sum-=a[idx];
+    h.pop_back();
+    if (subSeqSum(idx+1, target, a, sum, h)) return true;
+
+    return false // if nothing returned
+}
+*/
+
 void subSeqSum(int idx, int target, vector<int>&a, int sum, vector<int>&h){
     if (idx==a.size()){ 
         if (sum==target){
