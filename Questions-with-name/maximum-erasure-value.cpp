72788c6c813dc80f95878839cfe057380b851e01
Nishant Sharma <nishant-ai@outlook.com>
2022-06-12 19:32:32 +0530

    leetcode daily challenge 12 Jun 2022

--- Questions-with-name/maximum-erasure-value.cpp
+++ Questions-with-name/maximum-erasure-value.cpp
@@ -3,6 +3,34 @@
 #include "/Users/nishant/bits/stdc++.h"
 using namespace std;
 
+// Solution gave TLE
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        unordered_map <int, int> m;
+        int max_global = 0, max_local=0;
+        
+        for (int i=0; i<nums.size(); i++){
+            if (m[nums[i]]<1) { // If occur once
+                m[nums[i]]++; // track on map
+                max_local += nums[i]; // increment local maxima
+            }
+            
+            else if (m[nums[i]]==1) { // If repeats
+                i-=m.size();
+                m.clear();
+                max_local = 0;
+            }
+            
+            if (max_local>max_global) max_global=max_local;
+        }
+        
+        return max_global;
+    }
+};
+
+
+// Sliding Window and Map Approach
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
